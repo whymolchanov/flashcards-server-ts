9d726767779964f006abea202aa080dd1b749ff8
Yury Molchanov <0x4d59@gmail.com>
2023-03-14 09:53:37 +0100

    remove app controller and app service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,7 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { FlashcardModule } from './flashcard/flashcard.module';
+import { Module } from "@nestjs/common";
+import { FlashcardModule } from "./flashcard/flashcard.module";
 
 @Module({
   imports: [FlashcardModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
